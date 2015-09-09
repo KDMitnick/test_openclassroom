e383a8ba9e29408c1f1a0f2698830e4e32e2b3d8
KDMitnick <KDMitnick@KDMitnick.fr>
2015-09-09 20:33:00 +0200

    ajout de commentaire sur fonction.php

--- fonction.php
+++ fonction.php
@@ -8,6 +8,7 @@ function verif_mdp($_POST['mdp'], $_POST'[mdp2'])
 				if ($_POST['mdp'] ==$_POST['mdp2'])
 					{
 						if (preg_match("#^(^ ){3,120}#", $_POST['mdp']))
+//expression régulière pour verifier si le mot de passe n'a pas d'espace et fait de 3 $ 120 caractère
 							{
 								$mdp = $_POST['mdp'];
 								return $mdp;
